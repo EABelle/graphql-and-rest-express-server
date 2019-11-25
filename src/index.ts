4d14718a83d7e99cb22b13394d4aaa293c14296f
Esteban <esteban.bellegarde@gmail.com>
2019-11-24 22:47:37 -0300

    TSLint config and precommit hook

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import {Application} from "express";
+import {Application} from 'express';
 
 const express = require('express');
-const bodyParser = require("body-parser");
-import { userRouter } from "./api/router";
+const bodyParser = require('body-parser');
+import { userRouter } from './api/router';
 
 const mongoose = require('mongoose');
 const PORT = process.env.PORT;
@@ -10,25 +10,25 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 const app: Application = express();
 
-//middleware
-//body-parser
-app.use(bodyParser.urlencoded({extended:false}));
+// middleware
+// body-parser
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-//cors
-app.use(function (_req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+// cors
+app.use(function(_req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Authorization, Content-Type, Accept, X-Api-Key');
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
-    next()
-})
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    next();
+});
 
 app
     .use('/user', userRouter);
 
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
     .then(() => {
-        console.log("Connected to DB");
+        console.log('Connected to DB');
         app.listen(PORT, () => {
             console.log(`Server is listening on port: ${PORT}`);
         });
