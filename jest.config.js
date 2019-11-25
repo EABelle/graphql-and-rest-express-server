89be16e89ebbcf4cef6086a5a5db2a33f2fd9cce
Esteban <esteban.bellegarde@gmail.com>
2019-11-24 23:03:42 -0300

    Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    transform: { '^.+\\.ts$': 'ts-jest' },
+    testRegex: '(/__tests__/.*|(\\.|/)spec)\\.(js|ts)$',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    testPathIgnorePatterns: [
+        '/node_modules/'
+    ],
+    verbose: true,
+};
